5adaf3e17d457f8001a1beb242f51289b71629b0
Tim Vervoort <tim.vervoort@student.uhasselt.be>
2018-04-27 22:26:41 +0200

    Added swipe support

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # fs-gal
-Free to use full screen gallery for websites written in jQuery
+Free to use full screen gallery for websites.
+Plugin is written in jQuery.
 
 # How to use
-Include fs-gal.css and jQuery and fs-gal.js on your webpage.
-Create objects (div, img, a, ...) with class 'fs-gal' and data-attribute 'data-img'.
-The data-attribute 'data-img' is used to identify the image to open in the full screen gallery whend clicked.
+Include fs-gal.css, a recent jQuery and jQuery Mobile version and fs-gal.js on your webpage.
+Create objects (div, img, a, ...) with class 'fs-gal' and data-attribute 'data-url'.
+The data-attribute 'data-url' is used to identify the image to open in the full screen gallery whend clicked. Please note that this uri may not include spaces.
 When provided, the alt or title attribute is used to display the title of the image on the bottom of the gallery.
+Somwhere between the body tags, include the following snippet:
+```html
+<div class="fs-gal-view">
+  <h1></h1>
+  <img class="fs-gal-prev fs-gal-nav" src="img/prev.svg" alt="Previous picture" title="Previous picture" />
+  <img class="fs-gal-next fs-gal-nav" src="img/next.svg" alt="Next picture" title="Next picture" />
+  <img class="fs-gal-close" src="img/close.svg" alt="Close gallery" title="Close gallery" />
+</div>
+```
 
 # Navigation
-Visitors can click on objects with the data-img class to open the full screen gallery. Users can click on the previous and next icon or use the left and right arrow keys to navigate between images. The close button on top of the page or the ESC key is used to close the gallery.
+Visitors can click on objects with the fs-gal class to open the full screen gallery. Users can click on the previous and next icon or use the left and right arrow keys to navigate between images. The close button on top of the page or the ESC key is used to close the gallery.
--- js/fs-gal.js
+++ js/fs-gal.js
@@ -1,60 +1,90 @@
 /**
  * Author: Tim Vervoort - info@timvervoort.com
  * Licence: Free for commercial use
- * Last update: 8th November 2017 - v1.0
+ * Last update: 2th April 2018 - v1.2.2
  */
 $('document').ready(function() {
-  //Make gallery objects clickable
+
+  // Make gallery objects clickable
   $('.fs-gal').click(function() {
     fsGal_DisplayImage($(this));
   });
-  //Display gallery
+
+  // Display gallery
   function fsGal_DisplayImage(obj) {
-    //Clear navigation buttons
+
+    // Clear navigation buttons
     $('.fs-gal-view > .fs-gal-prev').fadeOut();
     $('.fs-gal-view > .fs-gal-next').fadeOut();
-    //Set current image
+
+    // Set current image
     var title = obj.attr('alt');
     if (!title || title == '') { title = obj.attr('title'); }
     $('.fs-gal-view > h1').text(title);
     if (!title || title == '') { $('.fs-gal-view > h1').fadeOut(); }
     else { $('.fs-gal-view > h1').fadeIn(); }
-    var img = obj.data('url');
+    var img = obj.attr('data-url');
     $('.fs-gal-view').css('background-image', 'url('+img+')');
-    //Create buttons
+
+    // Create buttons
     var current = $('.fs-gal').index(obj);
     var prev = current - 1;
     var next = current + 1;
     if (prev >= 0) {
-      $('.fs-gal-view > .fs-gal-prev').data('img-index', prev);
+      $('.fs-gal-view > .fs-gal-prev').attr('data-img-index', prev);
       $('.fs-gal-view > .fs-gal-prev').fadeIn();
     }
     if (next < $('.fs-gal').length) {
-      $('.fs-gal-view > .fs-gal-next').data('img-index', next);
+      $('.fs-gal-view > .fs-gal-next').attr('data-img-index', next);
       $('.fs-gal-view > .fs-gal-next').fadeIn();
     }
-    $('.fs-gal-view').fadeIn(); //Display gallery
+    $('.fs-gal-view').fadeIn(); // Display gallery
+    if (current == $('.fs-gal').length - 1)  { // Last image
+        $('.fs-gal-view > .fs-gal-next').attr('data-img-index', 0);
+        $('.fs-gal-view > .fs-gal-next').fadeIn();
+    }
+    else if (current == 0)  { // Last image
+        $('.fs-gal-view > .fs-gal-prev').attr('data-img-index', $('.fs-gal').length - 1);
+        $('.fs-gal-view > .fs-gal-next').fadeIn();
+    }
   }
-  //Gallery navigation
+
+  // Gallery navigation
   $('.fs-gal-view .fs-gal-nav').click(function() {
-    var index = $(this).data('img-index');
+    var index = $(this).attr('data-img-index');
     var img = $($('.fs-gal').get(index));
     fsGal_DisplayImage(img);
   });
-  //Close gallery
+
+  // Close gallery
   $('.fs-gal-view .fs-gal-close').click(function() {
     $('.fs-gal-view').fadeOut();
   });
-  //Keyboard navigation
+
+  // Keyboard navigation
   $('body').keydown(function(e) {
     if (e.keyCode == 37) {
-      $('.fs-gal-view .fs-gal-prev').click(); //Left arrow
+      $('.fs-gal-view .fs-gal-prev').click(); // Left arrow
     }
     else if(e.keyCode == 39) { // right
-      $('.fs-gal-view .fs-gal-next').click(); //Right arrow
+      $('.fs-gal-view .fs-gal-next').click(); // Right arrow
     }
     else if(e.keyCode == 27) { // right
-      $('.fs-gal-view .fs-gal-close').click(); //ESC
+      $('.fs-gal-view .fs-gal-close').click(); // ESC
     }
   });
+
+  $('.fs-gal-view').on('swipeleft', function() {
+    $('.fs-gal-view .fs-gal-prev').click(); // Previous img
+  });
+  $('.fs-gal-view').on('swiperight', function() {
+    $('.fs-gal-view .fs-gal-next').click(); // Next img
+  });
+  $('.fs-gal-view').on('swipedown', function() {
+    $('.fs-gal-view .fs-gal-close').click(); // Close gallery
+  });
+  $('.fs-gal-view').on('swipeup', function() {
+    $('.fs-gal-view .fs-gal-close').click(); // Close gallery
+  });
+
 });
